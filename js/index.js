67e7a2f014b36b93e72558d7069bf84fface02f9
xzw <460542390@qq.com>
2019-07-04 21:33:41 +0800

    7.04wanshang

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
 (function($){
 	var $dropdown = $('.dropdown');
+	//hover(鼠标移入时执行函数,鼠标移出时执行函数)
 	$dropdown.hover(function(){
+		//这里的this指向data-active="menu"然后做字符串的拼接
 		var activeClass = $(this).data('active') + "-active";
+		//添加到css这里的activeClass => menu-active
 		$(this).addClass(activeClass);
 	},function(){
 		var activeClass = $(this).data('active') + "-active";
